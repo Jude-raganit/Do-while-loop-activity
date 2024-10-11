de051ef3779c68c04b1336294dbe75351b036917
Jude-raganit <juderaganit56@gmail.com>
2024-10-11 19:06:53 +0800

    Create Passwordprompt.js

---
+++ Passwordprompt.js
@@ -0,0 +1,8 @@
+function ask password (){
+  let password= prompt("enter your password");
+  if (password){
+    alert ("incorrect password "+ "try again");
+  }else{
+    alert ("correct password"+ ,"access granted");
+  }
+}
